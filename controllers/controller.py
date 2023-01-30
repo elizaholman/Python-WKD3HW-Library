52864fdeb98796707c8ae72025def28af42c5b33
Eliza Peterson <121064539+ElizaPeterson@users.noreply.github.com>
2023-01-30 00:06:26 +0000

    second commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from app import app
-from models.library import books, add_new_book, remove_book
+from models.library import books, add_new_book, remove_book, check_in, check_out
 from models.book import Book
 
 @app.route("/library")
@@ -16,11 +16,22 @@ def add_book():
     book_title = request.form['title']
     book_author = request.form['author']
     book_genre =  request.form['genre']
-    new_book = Book(book_title, book_author, book_genre)
+    new_book = Book(book_title, book_author, book_genre, False)
     add_new_book(new_book)
     return render_template("index.html", title="Home", books=books)
 
 @app.route("/remove/<index>", methods=["POST"])
 def remove_book_from_library(index):
     remove_book(int(index))
+    return render_template("index.html", title="Home", books=books)
+
+@app.route("/checked_in/<index>", methods=["POST"])
+def check_in_book(index):
+    print("Hi")
+    check_in(int(index))
+    return render_template("index.html", title="Home", books=books)
+
+@app.route("/checked_out/<index>", methods=["POST"])
+def check_out_book(index):
+    check_out(int(index))
     return render_template("index.html", title="Home", books=books)
